b2123e945f438777329d5417f225511740bec8d8
emarubi <emarubi@gmail.com>
2021-01-23 17:18:01 +0100

    chore(route): article route modification with JOIN on user and activity tables

--- app/dataMappers/articleDataMapper.js
+++ app/dataMappers/articleDataMapper.js
@@ -7,8 +7,10 @@ module.exports = {
 
     async getAllArticles() {
         const result = await client.query('SELECT * FROM article');
+        // query with JOIN on activity tables
+        // const result = await client.query('SELECT * FROM article JOIN activity ON article.activity_id = activity.id');
         // query with JOIN on user and activity tables
-        // const result = await client.query('SELECT * FROM article JOIN "user" ON user_id ="user"."id" JOIN activity ON article.activity_id = activity.id');
+        const result = await client.query('SELECT * FROM article JOIN "user" ON user_id ="user"."id" JOIN activity ON article.activity_id = activity.id');
         return result.rows;
     },
 
