8100c32ace4b1a9bb100751e214c9e917f488d88
BijouDEFRE <fredericcreuseveau@gmail.com>
2021-01-25 13:52:04 +0100

    chore(BDD): insert field news_duration in article table

--- app/dataMappers/articleDataMapper.js
+++ app/dataMappers/articleDataMapper.js
@@ -10,7 +10,7 @@ module.exports = {
         // query with JOIN on activity tables
         // const result = await client.query('SELECT * FROM article JOIN activity ON article.activity_id = activity.id');
         // query with JOIN on user and activity tables
-        const result = await client.query('SELECT article.id, "article_title", "description", "picture_url", "price", "is_news", article.activity_id, "activity_name", "user_id", "first_name", "last_name", "shop_name", "city"  FROM article JOIN activity ON activity_id = activity.id JOIN "user" ON user_id ="user"."id"');
+        const result = await client.query('SELECT article.id, "article_title", "description", "picture_url", "price", "is_news", "news_duration", article.activity_id, "activity_name", "user_id", "first_name", "last_name", "shop_name", "city"  FROM article JOIN activity ON activity_id = activity.id JOIN "user" ON user_id ="user"."id"');
         return result.rows;
     },
 
@@ -29,8 +29,8 @@ module.exports = {
     },
 
     async createArticle(newArticle) {
-        const result = await client.query(`INSERT INTO "article"("article_title", "description", "picture_url", "price", "is_news", "user_id", "activity_id")
-        VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
+        const result = await client.query(`INSERT INTO "article"("article_title", "description", "picture_url", "price", "is_news", "user_id", "activity_id", "news_duration")
+        VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`,
         [
             newArticle.article_title,
             newArticle.description,
@@ -38,7 +38,8 @@ module.exports = {
             newArticle.price,
             newArticle.is_news,
             newArticle.user_id,
-            newArticle.activity_id
+            newArticle.activity_id,
+            newArticle.news_duration
      ]);
     return result.rows[0];
     },
