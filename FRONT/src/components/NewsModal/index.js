fd21cf48b3cc0fcaa9738622db939510c7e76f20
asmatar <deruelle.arthur@gmail.com>
2021-02-05 21:54:19 +0100

    modal done ready to merge

--- FRONT/src/components/NewsModal/index.js
+++ FRONT/src/components/NewsModal/index.js
@@ -35,22 +35,24 @@ const NewsModal = ({ news }) => {
       </section>
 
       <section>
-        <div className={`product-card modalBackground modalShowing-${modalState} active-modal-cart`}>
-          <div className="badge">Hot</div>
-          <div className="product-tumb">
-            <img src={news.picture_url} alt="news-image" />
-          </div>
-          <div className="product-details">
-            <span className="product-catagory">{news.activity_name}</span>
-            <h4><a href="">{news.article_title}</a></h4>
-            <p>{news.description}</p>
-            <div className="product-bottom-details">
-              <div className="product-price"><small>$96.00</small>${news.price}</div>
-              <div className="product-links">
-                <button onClick={() => manageState(!modalState)}>
-                  fermer la modale
-                </button>
-                <NavLink to={`/commercant/profil/${news.user_id}`}>Voir le profil de ce commercant</NavLink>
+        <div className={`modalBackground modalShowing-${modalState} product-modal`}>
+          <div className="container-modal">
+            <div className="badge-modal">Hot</div>
+            <div className="product-tumb-modal">
+              <img src={news.picture_url} alt="news-image-modal" className="modal-picture" />
+            </div>
+            <div className="product-details-modal">
+              <span className="product-catagory-modal">{news.activity_name}</span>
+              <h4><a href="">{news.article_title}</a></h4>
+              <p>{news.description}</p>
+              <div className="product-bottom-details-modal">
+                <div className="product-price-modal"><small>$96.00</small>${news.price}</div>
+                <div className="product-links-modal">
+                  <button onClick={() => manageState(!modalState)}>
+                    fermer la modale
+                  </button>
+                  <NavLink to={`/commercant/profil/${news.user_id}`}>Voir le profil de ce commercant</NavLink>
+                </div>
               </div>
             </div>
           </div>
